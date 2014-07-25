6f54b12302e70f9967fadd7ceef050e864ef359b
Lukas Klar <lukas.klar@mars.uni-freiburg.de>
2014-07-25 09:39:59 +0200

    d-gain experiment
    feedforward and a d-gain of 0.01.
    Visible changes in the send speed reference.

---
+++ 20140725_090758/load_hardware.lua
@@ -0,0 +1,75 @@
+#!/usr/bin/env rttlua-i
+require "deployment_helpers"
+for i,symbol in ipairs({"load_component",
+						"load_reporter",
+						"load_properties",
+						"get_property",
+						"set_property",
+						"sleep"}) do
+	_G[symbol] = deployment_helpers[symbol]
+end
+
+PLANEPOWER="../../"
+PROPERTIES=PLANEPOWER.."properties/"
+
+libraryNames={"siemensActuators",
+				"siemensSensors",
+				"lineAngleSensor2",
+				"armboneLisaSensors"}
+classNames={"SiemensActuators",
+				"SiemensSensors",
+				"LineAngleSensor2",
+				"ArmboneLisaSensors"}
+function deepcopy(liist)
+	newlist = {}
+	for i,symbol in ipairs(liist) do
+		newlist[i] = symbol
+	end
+	return newlist
+end
+instanceNames=deepcopy(libraryNames)
+
+function appendSimulator(listOfNames)
+	for i,symbol in ipairs(listOfNames) do
+		listOfNames[i] = listOfNames[i].."Simulator" 
+	end
+end
+if useSimulators then 
+	appendSimulator(libraryNames)
+	appendSimulator(classNames)
+	-- instance Names stay the same for compatibility with everything else!
+end
+
+-- Load up the Carousel 2 hardware that interfaces with sensors and hardware
+for i=1,#libraryNames do
+	load_component(libraryNames[i],classNames[i],instanceNames[i])
+end
+
+-- Load up a carouselSimulator component regardless of we are doing simulations,
+-- since we also use it for the steady state lookup tables.
+load_component("carouselSimulator","CarouselSimulator","carouselSimulator")
+--function lookup_steady_state_speed(elevation)
+	--carouselSimulator:lookup_steady_state_speed(elevation)
+--end
+--function lookup_steady_state_elevation(speed)
+	--carouselSimulator:lookup_steady_state_elevation(speed)
+--end
+carouselSimulator:configure()
+lookup_steady_state_elevation = carouselSimulator:getOperation('lookup_steady_state_elevation')
+
+----------------- Set Priorities and activities
+
+-- All of these components trigger themselves once started the first time.
+deployer:setActivityOnCPU("siemensSensors", 0.0, sensorPrio, scheduler,quietCore)
+deployer:setActivityOnCPU("lineAngleSensor2", 0.0, sensorPrio, scheduler,quietCore)
+deployer:setActivityOnCPU("armboneLisaSensors", 0.0, sensorPrio, scheduler,quietCore)
+deployer:setActivityOnCPU("siemensActuators", 0.0, sensorPrio, scheduler,quietCore)
+
+--------------- Configure and start the components
+
+for i=1,#instanceNames do
+	_G[instanceNames[i]]:configure()
+	_G[instanceNames[i]]:start()
+end
+
+
