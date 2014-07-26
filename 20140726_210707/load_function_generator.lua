4d0184c3823aa24ed2e1610d159a5541a7610fa8
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2014-07-26 21:30:10 +0200

    tuned PID gains, got nice gains! observed a bit of saturation which was not seen in live plots by greg

---
+++ 20140726_210707/load_function_generator.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/env rttlua-i
+require "deployment_helpers"
+for i,symbol in ipairs({"load_component",
+						"load_reporter",
+						"load_properties",
+						"store_properties",
+						"get_property",
+						"set_property",
+						"sleep"}) do
+	_G[symbol] = deployment_helpers[symbol]
+end
+
+PLANEPOWER="../../"
+
+load_component("functionGenerator","FunctionGenerator","functionGenerator")
+
+freeRunningFunctionGenerator = true
+
+----------------- Set Priorities and activities
+if freeRunningFunctionGenerator then
+	updateFrequency = 40
+	deployer:setActivityOnCPU("functionGenerator", 1.0/updateFrequency, controllerPrio, scheduler,quietCore)
+else
+	deployer:setActivityOnCPU("functionGenerator", 0.0, controllerPrio, scheduler,quietCore)
+end
+
+cp = rtt.Variable("ConnPolicy")
+
+if runningOpenLoop then
+	deployer:connect("functionGenerator.data","siemensActuators.controls",cp)
+end
+
+if not freeRunningFunctionGenerator then
+	deployer:connect("siemensSensors.triggerOut","functionGenerator.triggerIn",cp)
+end
+
+--------------- Configure and start the components
+functionGenerator:configure()
+
